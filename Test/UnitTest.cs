1973703f6df0cb1ccb9a84510ab49d3c82e704ec
Victor Davydenko <vida@simcorp.com>
2020-05-25 12:11:05 +0200

    add UnitTest and packages.config

---
+++ Test/UnitTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestArray.Test
+{
+    [TestClass]
+    public class UnitTest
+    {
+        private IMatrix matrix;
+        [TestMethod]
+        public void TestArray_OK()
+        {
+            matrix = new MatrixList(9, 4);
+
+            Assert.AreEqual("1", 1.ToString());
+        }
+
+        [TestMethod]
+        public void TestMatrix_OK()
+        {
+            matrix = new MartixArr(9, 4);
+
+            Assert.AreEqual("1", 1.ToString());
+        }
+
+        [TestMethod]
+        public void TestMatrix_Fail()
+        {
+           
+            matrix = new MartixArr(9, 4);
+            var matrixA = new MartixArr(9, 4);
+            Assert.AreEqual(matrix, matrixA);
+        }
+    }
+}
